9b62ebcaeaf89800d28b06cd736c6ebcb0d58687
Nodirjon Ro'zimurodov <82306944+Nodirjon2303@users.noreply.github.com>
2021-09-23 22:53:36 -0700

    last2

--- Shahzod_aka_project/settings.py
+++ Shahzod_aka_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Shahzod_aka_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3lav3lsm9inul',
+        'USER': 'vdqjmidlgjuiyh',
+        'PASSWORD': 'bdddef030caf1ad7bcbd353813a7f7d16b6e9100b334474064e62bf3e15609d8',
+        'HOST': 'ec2-34-197-135-44.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
